ad3fcff3788502b477ffafb63558d5c630d8cc2c
PriyamDalmia <dalmia.priyam@outlook.com>
2024-10-24 15:03:50 +1100

    new job submission script

---
+++ scripts/submit_spartan_job.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# exit on first fail
+set -e
+
+# Usage: ./carry_over_changes.sh <name> <branch_a> <branch_b>
+# Example: ./carry_over_changes.sh "John Doe" "a" "b"
+
+SUBMISSION_BRANCH="dev"
+JOBS_BRANCH="jobs-spartan"
--- scripts/sync_spartan_branch.sh
+++ scripts/sync_spartan_branch.sh
@@ -29,7 +29,7 @@ fi
 REMOTE_SUB=$(git ls-remote --heads origin "$SUBMISSION_BRANCH")
 REMOTE_JOBS=$(git ls-remote --heads origin "$JOBS_BRANCH")
 
-if [-z "$REMOTE_SUB"]; then 
+if [ -z "$REMOTE_SUB" ]; then 
     echo "Error: remote for branch \'$SUBMISSION_BRANCH\' not found"
     exit 1
 fi 
@@ -37,4 +37,24 @@ fi
 # Get the current date in the format YYYY-MM-DD
 DATE=$(date +"%Y-%m-%d")
 
+# stash any local changes 
+git stash push -m "Stashing local changes before merging"
+
+# checkout JOB_BRANCH 
+git checkout $JOBS_BRANCH 
+
+# merge SUBMISSION_BRANCH to the JOBS_BRANCH; apply stash 
+git merge --no-ff $SUMBISSION_BRANCH --strategy-option theirs
+git stash apply 
+
+# git changes to remote 
+git push origin $JOBS_BRANCH --force 
+
+# go back to the old branch 
+git chectout $SUBMISSION_BRANCH 
+
+# reapply the stash 
+git stash apply 
+
 echo "Script ran succesfully!!"
+
